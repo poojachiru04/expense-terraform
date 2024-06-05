92d8f83767d32433514590bae52a2034d3c38d6c
pooja <poojaskumara@gmail.com>
2024-06-05 17:17:26 +0530

    add expense terraform to pipelines

--- run.sh
+++
@@ -1,2 +0,0 @@
-terraform init -backend-config=env-${env}/state.tfvars
-terraform ${action} -auto-approve -var-file=env-${env}/main.tfvars -var vault_token=${vault_token} -var SSH_PASSWORD=${SSH_PASSWORD}
