bebe51ba9f9dfb698b7a4c47197dcb56071f67e7
pooja <poojaskumara@gmail.com>
2024-06-05 19:33:30 +0530

    add expense terraform to pipelines

--- data.tf
+++ data.tf
@@ -1,10 +1,10 @@
+data "aws_route53_zone" "main" {
+  name         = "poodevops.online"
+  private_zone = false
+}
+
 data "aws_ami" "ami" {
   most_recent = true
   name_regex  =  "RHEL-9-DevOps-Practice"
   owners      = ["973714476881"]
-}
-
-data "aws_route53_zone" "main" {
-  name         = "poodevops.online"
-  private_zone = false
 }
--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "frontend" {
   depends_on = [module.backend]
-  source     = "git::https://github.com/raghudevopsb79/tf-module-app.git"
+  source     = "git::https://github.com/poojachiru04/tf-module-app.git"
 
   instance_type = var.components["frontend"]["instance_type"]
   name          = var.components["frontend"]["name"]
@@ -10,7 +10,7 @@ module "frontend" {
 
 module "backend" {
   depends_on = [module.mysql]
-  source     = "git::https://github.com/raghudevopsb79/tf-module-app.git"
+  source     = "git::https://github.com/poojachiru04/tf-module-app.git"
 
   instance_type = var.components["backend"]["instance_type"]
   name          = var.components["backend"]["name"]
@@ -19,7 +19,7 @@ module "backend" {
 }
 
 module "mysql" {
-  source = "git::https://github.com/raghudevopsb79/tf-module-app.git"
+  source = "git::https://github.com/poojachiru04/tf-module-app.git"
 
   instance_type = var.components["mysql"]["instance_type"]
   name          = var.components["mysql"]["name"]
