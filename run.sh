de6f3fe689ce444f92f2e68cacb580803d49ea54
pooja <poojaskumara@gmail.com>
2024-06-15 14:44:19 +0530

    use filters to monior the prometheus

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 terraform init -backend-config=env-${env}/state.tfvars
-terraform apply -auto-approve -var-file=env-${env}/main.tfvars -var vault_token=${vault_token} -var SSH_PASSWORD=${SSH_PASSWORD}
+terraform ${action} -auto-approve -var-file=env-${env}/main.tfvars -var vault_token=${vault_token} -var SSH_PASSWORD=${SSH_PASSWORD}
