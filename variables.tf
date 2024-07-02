6dfd5e1a55e920c4805f3301b0b337685ca7e198
pooja <poojaskumara@gmail.com>
2024-07-02 15:52:47 +0530

    kubernetes

--- variables.tf
+++ variables.tf
@@ -1,9 +1,6 @@
-variable "components" {}
-variable "env" {}
-variable "vpc" {}
-variable "vault_token" {}
 variable "SSH_PASSWORD" {}
-variable "prometheus_server" {}
+variable "vpc" {}
+variable "env" {}
+variable "vault_token" {}
 variable "tags" {}
-
 
